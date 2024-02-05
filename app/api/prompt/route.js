62cb2f75b35457fd20b60b0f0b82587d07094817
Mirza Faiz <kaifsid774@gmail.com>
2024-02-06 01:00:37 +0530

    sixth

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,11 +1,17 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { cookies } from "next/headers";
 
 export const GET = async (req) => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: { "Set-Cookie": `token=${token}` },
+    });
   } catch (e) {
     console.log(e);
   }
